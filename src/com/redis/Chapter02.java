8b2b656507b6915dcb84d01fbd6e7826e0f39ba4
kayang0000 <18410422493@163.com>
2019-07-05 17:50:41 +0800

    chapter02 init -2

--- src/com/redis/Chapter02.java
+++ src/com/redis/Chapter02.java
@@ -9,8 +9,8 @@ import java.util.UUID;
 
 public class Chapter02 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        new Chapter02().run();
     }
 
     public void run() throws InterruptedException {
