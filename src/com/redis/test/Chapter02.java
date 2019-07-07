74f19f728dc8026e1177e85641740774054f41ed
kqyang0000 <18410422493@163.com>
2019-07-07 16:47:35 +0800

    add shopping cart

--- src/com/redis/test/Chapter02.java
+++ src/com/redis/test/Chapter02.java
@@ -4,6 +4,7 @@ import com.redis.common.Base;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Chapter02 extends Base {
@@ -68,9 +69,54 @@ public class Chapter02 extends Base {
     /**
      * 测试购物车
      */
-    public void testShoppingCartCookies() {
+    public void testShoppingCartCookies() throws InterruptedException {
         printer("\n----- testShoppingCartCookies -----");
         String token = getToken();
+        /*
+         * 刷新token
+         */
+        printer("We'll refresh our session...");
+        updateToken(token, "username", "itemY");
+
+        /*
+         * 添加一个商品到购物车
+         */
+        printer("And add an item to the shopping cart");
+        addToCart(token, "itemY", 3);
+
+        /*
+         * 获取购物车中所有商品
+         */
+        Map<String, String> items = conn.hgetAll("cart:" + token);
+        printer("Our shopping cart currently has:");
+        for (Map.Entry<String, String> entry : items.entrySet()) {
+            printer(entry.getKey() + ":" + entry.getValue());
+        }
+        printer();
+        assert items.size() >= 1;
+
+        /*
+         * 清理购物车
+         */
+        printer("Let's clean out our sessions and carts");
+        CleanFullSessionThread thread = new CleanFullSessionThread(10000000);
+        thread.start();
+        Thread.sleep(1000);
+        thread.quit();
+        Thread.sleep(2000);
+        if (thread.isAlive()) {
+            throw new RuntimeException("The clean sessions thread is still alive!");
+        }
+
+        /*
+         * 获取购物车中所有商品
+         */
+        items = conn.hgetAll("cart:" + token);
+        printer("Our shopping cart now contains");
+        for (Map.Entry<String, String> entry : items.entrySet()) {
+            printer(entry.getKey() + ":" + entry.getValue());
+        }
+        assert items.size() == 0;
     }
 
     /**
@@ -139,6 +185,66 @@ public class Chapter02 extends Base {
                 conn.del(sessionKeys.toArray(new String[sessionKeys.size()]));
                 conn.hdel("login:", tokens);
                 conn.zrem("recent:", tokens);
+            }
+        }
+    }
+
+    /**
+     * 添加商品到购物车
+     *
+     * @param session
+     * @param item
+     * @param count
+     */
+    public void addToCart(String session, String item, int count) {
+        if (count <= 0) {
+            conn.hdel("cart:" + session, item);
+        } else {
+            conn.hset("cart:" + session, item, String.valueOf(count));
+        }
+    }
+
+    /**
+     * 清理购物车线程
+     */
+    public class CleanFullSessionThread extends Thread {
+        private int limit;
+        private boolean quit;
+
+        public CleanFullSessionThread(int limit) {
+            this.limit = limit;
+        }
+
+        public void quit() {
+            quit = true;
+        }
+
+        @Override
+        public void run() {
+            while (!quit) {
+                long size = conn.zcard("recent:");
+                if (size <= limit) {
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+
+                    }
+                    continue;
+                }
+
+                long endIndex = Math.min(size - limit, 100);
+                Set<String> sessionSet = conn.zrange("recent:", 0, endIndex - 1);
+                String[] sessions = sessionSet.toArray(new String[sessionSet.size()]);
+
+                List<String> sessionKeys = new LinkedList<>();
+                for (String session : sessions) {
+                    sessionKeys.add("viewed:" + session);
+                    sessionKeys.add("cart:" + session);
+                }
+
+                conn.del(sessionKeys.toArray(new String[sessionKeys.size()]));
+                conn.hdel("login:", sessions);
+                conn.zrem("recent:", sessions);
             }
         }
     }
