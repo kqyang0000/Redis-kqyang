43eaec7b53d51077538e8d84f4884e493e44e8b1
kayang0000 <18410422493@163.com>
2019-07-10 17:43:24 +0800

    operator list type of redis

--- src/com/redis/chapter/Chapter03.java
+++ src/com/redis/chapter/Chapter03.java
@@ -7,6 +7,7 @@ public class Chapter03 extends Base {
 
     public static void main(String[] args) {
         new Chapter03().new STRING();
+        new Chapter03().new LIST();
     }
 
     /**
@@ -44,6 +45,42 @@ public class Chapter03 extends Base {
             conn.bitcount(key, 0, 0);
             // 12.按位运算操作（AND OR XOR NOT），返回运算后的字符长度
             conn.bitop(BitOP.AND, "s-key", "s-key1", "s-key2");
+        }
+    }
+
+    /**
+     * 列表常用操作
+     */
+    public class LIST {
+        private final String key = "list-key";
+
+        public LIST() {
+            run();
+        }
+
+        public void run() {
+            // 1.将一个或多个值推入列表的左端，返回列表长度
+            conn.lpush(key, new String[]{"a", "b", "c", "d"});
+            // 2.将一个或多个值推入列表的右端，返回列表长度
+            conn.rpush(key, new String[]{"e", "f", "g", "h"});
+            // 3.移除并返回列表最左端的元素
+            conn.lpop(key);
+            // 4.移除并返回列表最右端的元素
+            conn.rpop(key);
+            // 5.返回列表中偏移量为offset的元素
+            conn.lindex(key, -1);
+            // 6.返回范围内的列表元素，包含起始终止范围的元素
+            conn.lrange(key, 0, -1);
+            // 7.队列表进行修剪，保留起至终止范围内的元素
+            conn.ltrim(key, 0, 4);
+            // 8.从第一个非空列表中弹出位于最左侧的元素，或者在timeout秒之内阻塞并等待可弹出的元素出现（有值则立刻弹出，否则阻塞等待）
+            conn.blpop(20, key);
+            // 9.从第一个非空列表中弹出位于最右侧的元素，或者在timeout秒之内阻塞并等待可弹出的元素出现（有值则立刻弹出，否则阻塞等待）
+            conn.brpop(20, key);
+            // 10.从列表一中弹出位于最右端的元素，然后将这个元素推入列表二的最左端，并返回这个元素
+            conn.rpoplpush("l1", "l2");
+            // 11.从列表一中弹出位于最右端的元素，然后将这个元素推入列表二的最左端，并返回这个元素，或者在timeout秒之内阻塞并等待可弹出的元素出现（有值则立刻弹出，否则阻塞等待）
+            conn.brpoplpush("l1", "l2", 20);
         }
     }
 }
